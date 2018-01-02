934d83690ce721052358110d3537dbdb3b69d1ab
Adrien <verblodung@gmail.com>
2018-01-02 11:46:25 -0500

    feat(dependencies): add react-navigation, native-base

--- App.js
+++ App.js
@@ -1,23 +1,80 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {
+  Container,
+  Header,
+  Title,
+  Left,
+  Icon,
+  Right,
+  Button,
+  Body,
+  Content,
+  Text,
+  Card,
+  CardItem
+} from 'native-base'
+// import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              transparent
+              onPress={() => this.props.navigation.navigate('DrawerOpen')}
+            >
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>HomeScreen</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content padder>
+          <Card>
+            <CardItem>
+              <Body>
+                <Text>Chat App to talk some awesome people!</Text>
+              </Body>
+            </CardItem>
+          </Card>
+          <Button
+            full
+            rounded
+            dark
+            style={{ marginTop: 10 }}
+            onPress={() => this.props.navigation.navigate('Chat')}
+          >
+            <Text>Chat With People</Text>
+          </Button>
+          <Button
+            full
+            rounded
+            primary
+            style={{ marginTop: 10 }}
+            onPress={() => this.props.navigation.navigate('Profile')}
+          >
+            <Text>Goto Profiles</Text>
+          </Button>
+        </Content>
+      </Container>
+      // <View style={styles.container}>
+      //   <Text>Open up App.js to start working on your app!</Text>
+      //   <Text>Changes you make will automatically reload.</Text>
+      //   <Text>Shake your phone to open the developer menu.</Text>
+      // </View>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
