baa00e325e22bb391d33ec4c8b189e9a1bf3599b
Adrien Lacquemant <verblodung@gmail.com>
2018-01-17 14:25:08 -0500

    feat: adding one more request to API after the end of the set timeout

--- App.js
+++ App.js
@@ -56,14 +56,19 @@ class HomeScreen extends React.Component {
   }
 
   handleSearch(text) {
-    if (text.length > 2 && !this.state.isLoading) {
-      this.setState({ searchInput: text, isLoading: true })
+    function placesAutocomplete(text) {
       fetch(
         'https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyBa2s7Y4_idfCl6UQOhAOJtasI01mQwv0g&input=' +
           text
       ).then(console.log)
+    }
+
+    if (text.length > 2 && !this.state.isLoading) {
+      this.setState({ searchInput: text, isLoading: true })
+      placesAutocomplete(text)
       setTimeout(() => {
         this.setState({ isLoading: false })
+        placesAutocomplete(this.state.searchInput)
       }, 2000)
     } else {
       this.setState({ searchInput: text })
@@ -181,58 +186,6 @@ class HomeScreen extends React.Component {
                 <Text />
                 <Text />
                 <Text />
-                {/* <GooglePlacesAutocomplete
-              placeholder="Search"
-              minLength={2} // minimum length of text to search
-              autoFocus={true}
-              returnKeyType={'search'} // Can be left out for default return key https://facebook.github.io/react-native/docs/textinput.html#returnkeytype
-              listViewDisplayed="auto" // true/false/undefined
-              fetchDetails={true}
-              renderDescription={row => row.description} // custom description render
-              onPress={(data, details = null) => {
-                // 'details' is provided when fetchDetails = true
-                console.log(data, details)
-              }}
-              getDefaultValue={() => ''}
-              query={{
-                // available options: https://developers.google.com/places/web-service/autocomplete
-                key: 'AIzaSyBa2s7Y4_idfCl6UQOhAOJtasI01mQwv0g',
-                language: 'en', // language of the results
-                types: 'establishment' // default: 'geocode'
-              }}
-              styles={{
-                textInputContainer: {
-                  width: '100%'
-                },
-                description: {
-                  fontWeight: 'bold'
-                },
-                predefinedPlacesDescription: {
-                  color: '#1faadb'
-                }
-              }}
-              currentLocation={true} // Will add a 'Current location' button at the top of the predefined places list
-              currentLocationLabel="Current location"
-              nearbyPlacesAPI="GoogleReverseGeocoding" // Which API to use: GoogleReverseGeocoding or GooglePlacesSearch
-              GoogleReverseGeocodingQuery={
-                {
-                  // available options for GoogleReverseGeocoding API : https://developers.google.com/maps/documentation/geocoding/intro
-                }
-              }
-              GooglePlacesSearchQuery={{
-                // available options for GooglePlacesSearch API : https://developers.google.com/places/web-service/search
-                rankby: 'distance',
-                types: 'food'
-              }}
-              filterReverseGeocodingByTypes={[
-                'locality',
-                'administrative_area_level_3'
-              ]} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
-              predefinedPlaces={[homePlace, workPlace]}
-              debounce={200} // debounce the requests in ms. Set to 0 to remove debounce. By default 0ms.
-              // renderLeftButton={() => <Image source={require('./i.png')} />}
-              // renderRightButton={() => <Text>Custom text after the input</Text>}
-            /> */}
               </View>
             </View>
           </Content>
